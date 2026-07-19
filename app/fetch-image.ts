8696cffdfc35d9d9bd3a81da05931c24ed429ee1
ken goto <houtengjian31@gmail.com>
2026-07-19 11:07:29 +0900

    キャッシュオプションの無効化

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -8,6 +8,7 @@ type Image = {
 export async function fetchImage(): Promise<Image>{
     const res = await fetch("https://api.thecatapi.com/v1/images/search", {
         headers: {"x-api-key":CAT_API_KEY},
+        cache: "no-store", // 毎回新しい画像を取得する
     });
     const images = await res.json();
     console.log("画像情報を取得しました",images);
